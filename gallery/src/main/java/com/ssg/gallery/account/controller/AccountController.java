677415ab7868d69aae3c805526c052c3ef66d5cd
dynii <dyniiyeyo@naver.com>
2025-11-27 15:09:27 +0900

    251127 lecture

--- gallery/src/main/java/com/ssg/gallery/account/controller/AccountController.java
+++ gallery/src/main/java/com/ssg/gallery/account/controller/AccountController.java
@@ -2,25 +2,32 @@ package com.ssg.gallery.account.controller;
 
 import com.ssg.gallery.account.dto.AccountJoinRequest;
 import com.ssg.gallery.account.dto.AccountLoginRequest;
+import com.ssg.gallery.account.etc.AccountConstants;
 import com.ssg.gallery.account.helper.AccountHelper;
+import com.ssg.gallery.block.service.BlockService;
+import com.ssg.gallery.common.util.HttpUtils;
+import com.ssg.gallery.common.util.TokenUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor()
 @RequestMapping("v1")
 public class AccountController {
     private final AccountHelper accountHelper;
+    private final BlockService blockService; // ① 스프링 컨테이너에 의해 의존성 주입될 토큰 차단 서비스 필드
 
     @PostMapping("/api/account/join")
-    // 회원가입
     public ResponseEntity<?> join(@RequestBody AccountJoinRequest joinReq) {
-        // 입력값이 비어있다면
-        if (joinReq.getName() == null || joinReq.getLoginId() == null || joinReq.getLoginPw() == null) {
+        // 입력 값이 비어 있다면
+        if (!StringUtils.hasLength(joinReq.getName()) || !StringUtils.hasLength(joinReq.getLoginId()) || !StringUtils.hasLength(joinReq.getLoginPw())) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
@@ -30,26 +37,53 @@ public class AccountController {
 
     // 로그인
     @PostMapping("/api/account/login")
-    public ResponseEntity<?> login(HttpServletRequest request, HttpServletResponse response, @RequestBody AccountLoginRequest loginReq) {
-        if(loginReq.getLoginId() == null || loginReq.getLoginPw() == null) {
+    public ResponseEntity<?> login(HttpServletRequest req, HttpServletResponse res, @RequestBody AccountLoginRequest loginReq) {
+        // 입력 값이 비어 있다면
+        if (!StringUtils.hasLength(loginReq.getLoginId()) || !StringUtils.hasLength(loginReq.getLoginPw())) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        String output = accountHelper.login(loginReq, request, response);
-        if (output == null) {
+        String output = accountHelper.login(loginReq, req, res);
+
+        if (output == null) { // 로그인 실패 시
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
         return new ResponseEntity<>(output, HttpStatus.OK);
     }
 
     @GetMapping("/api/account/check")
-    public ResponseEntity<?> check(HttpServletRequest request) {
-        return new ResponseEntity<>(accountHelper.isLoggedIn(request), HttpStatus.OK);
+    public ResponseEntity<?> check(HttpServletRequest req) {
+        return new ResponseEntity<>(accountHelper.isLoggedIn(req), HttpStatus.OK);
     }
 
     @PostMapping("/api/account/logout")
-    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
-        accountHelper.logout(request, response);
+    public ResponseEntity<?> logout(HttpServletRequest req, HttpServletResponse res) {
+        accountHelper.logout(req, res);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    // ② 액세스 토큰을 재발급하는 메서드. @GetMapping 애너테이션을 지정하여 HTTP GET요청을 매핑하고, 연결 경로로 /api/account/token 을 지정한다.
+//   쿠키의 리프레시 토큰을 조회하고 이 값이 유효하다면 이 토큰을 활용하여 액세스 토큰을 발급하고 리턴한다.
+
+    @GetMapping("/api/account/token")
+    public ResponseEntity<?> regenerate(HttpServletRequest req) {
+        String accessToken = "";
+        String refreshToken = HttpUtils.getCookieValue(req, AccountConstants.REFRESH_TOKEN_NAME);
+
+        // 리프레시 토큰이 유효하다면
+        if (StringUtils.hasLength(refreshToken) && TokenUtils.isValid(refreshToken) && !blockService.has(refreshToken)) {
+            // 리프레시 토큰의 내부 값 조회
+            Map<String, Object> tokenBody = TokenUtils.getBody(refreshToken);
+
+            // 리프레시 토큰의 회원 아이디 조회
+            Integer memberId = (Integer) tokenBody.get(AccountConstants.MEMBER_ID_NAME);
+
+            // 액세스 토큰 발급
+            accessToken = TokenUtils.generate(AccountConstants.ACCESS_TOKEN_NAME, AccountConstants.MEMBER_ID_NAME, memberId, AccountConstants.ACCESS_TOKEN_EXP_MINUTES);
+        }
+
+        return new ResponseEntity<>(accessToken, HttpStatus.OK);
+    }
 }
+
--- gallery/src/main/java/com/ssg/gallery/member/service/BaseMemberService.java
+++ gallery/src/main/java/com/ssg/gallery/member/service/BaseMemberService.java
@@ -1,5 +1,6 @@
 package com.ssg.gallery.member.service;
 
+import com.ssg.gallery.common.util.HashUtils;
 import com.ssg.gallery.member.entity.Member;
 import com.ssg.gallery.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,13 +15,37 @@ public class BaseMemberService implements MemberService {
 
 
     @Override
-    public void save(String name, String loginId, String loginPw) {
-        memberRepository.save(new Member(name, loginId, loginPw));
+    public void save(String name, String loginId, String loginPw) { // ②
+        // 솔트 생성
+        String loginPwSalt = HashUtils.generateSalt(16);
+
+        // 입력 패스워드에 솔트를 적용
+        String loginPwSalted = HashUtils.generateHash(loginPw, loginPwSalt);
+
+        // 회원 데이터 저장
+        memberRepository.save(new Member(name, loginId, loginPwSalted, loginPwSalt));
     }
 
     @Override
     public Member find(String loginId, String loginPw) {
-        Optional<Member> member = memberRepository.findByLoginIdAndLoginPw(loginId, loginPw);
-        return member.orElse(null); // 회원 데이터가 있으면 해당 멤버를 리턴, 없으면 Null
+        // 로그인 아이디로 회원 조회
+        Optional<Member> memberOptional = memberRepository.findByLoginId(loginId);
+        // 회원 데이터가 있으면
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+
+            // 솔트 조회
+            String loginPwSalt = memberOptional.get().getLoginPwSalt();
+
+            // 입력 패스워드에 솔트를 적용
+            String loginPwSalted = HashUtils.generateHash(loginPw, loginPwSalt);
+
+            // 저장된 회원 로그인 패스워드와 일치한다면
+            if (member.getLoginPw().equals(loginPwSalted)) {
+                return member;
+            }
+        }
+
+        return null;
     }
 }
