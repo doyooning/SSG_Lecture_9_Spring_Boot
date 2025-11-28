fc218d6e024da8a36a7d3492eb9f4ccab5c9c73e
dynii <dyniiyeyo@naver.com>
2025-11-28 17:57:39 +0900

    deploy practice

--- gallery-deploy/src/main/java/com/ssg/gallery/common/controller/MainController.java
+++ gallery-deploy/src/main/java/com/ssg/gallery/common/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping(value = {"/", "{path:[^.]*}", "{path1:[^.]*}/{path2:[^.]*}"})
+    // 매핑주소 확인
     public String index() {
         return "/index.html";
     }
